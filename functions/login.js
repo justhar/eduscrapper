059ebe3597078a526386cd71e3dd1ab4a948effe
haroki <harokifathan@gmail.com>
2025-05-06 09:54:01 +0700

    last

--- functions/login.js
+++ functions/login.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer-core');
 
 exports.handler = async (event, context) => {
-
     const username = JSON.parse(event.body).username;
     const password = JSON.parse(event.body).password;
     const school = JSON.parse(event.body).school;
@@ -9,37 +8,39 @@ exports.handler = async (event, context) => {
     const browser = await puppeteer.connect({
         browserWSEndpoint: "wss://chrome.browserless.io?token=SG6HTbe2tAItQOb3b7dd51663f011f2548fccaa457",
     });
-    
+
     const page = await browser.newPage();
 
     await page.goto(`https://${school}.edunav.net/login`, { waitUntil: 'networkidle2' });
 
-    await page.locator('input[name="username"]').fill(username)
-    await page.locator('input[name="password"]').fill(password)
-    await page.locator('button[type="submit"]').click()
+    // Use Puppeteer's methods to interact with the page
+    await page.type('input[name="username"]', username);
+    await page.type('input[name="password"]', password);
+    await page.click('button[type="submit"]');
 
     const navigationPromise = page.waitForNavigation();
     await navigationPromise;
     const url = page.url();
 
-    if(url === `https://${school}.edunav.net/`) {
-        const name = 'body > div.page > div > div > div.col-lg-8 > div.row > div:nth-child(1) > div.widget.widget-shadow.mb-15 > div.widget-header.bg-primary.p-15.clearfix > div.font-size-18.mt-10';
-        const classs = 'body > div.page > div > div > div.col-lg-8 > div.row > div:nth-child(1) > div.widget.widget-shadow.mb-15 > div.widget-header.bg-primary.p-15.clearfix > div.font-size-14';
-        const extractedName = await page.$eval(name, el => el.textContent.trim());
-        const extractedClass = await page.$eval(classs, el => el.textContent.trim());
+    if (url === `https://${school}.edunav.net/`) {
+        const nameSelector = 'body > div.page > div > div > div.col-lg-8 > div.row > div:nth-child(1) > div.widget.widget-shadow.mb-15 > div.widget-header.bg-primary.p-15.clearfix > div.font-size-18.mt-10';
+        const classSelector = 'body > div.page > div > div > div.col-lg-8 > div.row > div:nth-child(1) > div.widget.widget-shadow.mb-15 > div.widget-header.bg-primary.p-15.clearfix > div.font-size-14';
+        const extractedName = await page.$eval(nameSelector, el => el.textContent.trim());
+        const extractedClass = await page.$eval(classSelector, el => el.textContent.trim());
         const text = {
             name: extractedName,
             class: extractedClass
-        }
+        };
         await browser.close();
         return {
             statusCode: 200,
             body: JSON.stringify(text)
         };
     } else {
+        await browser.close();
         return {
             statusCode: 401,
             body: JSON.stringify({ message: 'Invalid credentials' })
         };
     }
-}
+};
